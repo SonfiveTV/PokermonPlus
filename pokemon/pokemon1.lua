ed20edfe1a10d76e793d19dbe1e7cea9afd8f5ec
SonfiveTV <sonfive.jt@gmail.com>
2025-06-28 08:06:11 +0100

    Meltan energy fix

--- pokemon/pokemon1.lua
+++ pokemon/pokemon1.lua
@@ -28,142 +28,136 @@
 --     end,
 -- }
 
--- local duskull = {
---   name = "duskull", 
---   poke_custom_prefix = "sonfive",
---   pos = {x = 6, y = 10}, 
---   config = {extra = {mult_mod = 5, mult = 0}, evo_rqmt = 25},
---   loc_vars = function(self, info_queue, card)
---     type_tooltip(self, info_queue, card)
---     local mult = card.ability.extra.mult
---     return {vars = {mult, card.ability.extra.mult_mod}}
---   end,
---   rarity = 2, 
---   cost = 5, 
---   stage = "Basic", 
---   ptype = "Psychic",
---   atlas = "pokedex_3",
---   blueprint_compat = true,
---   calculate = function(self, card, context)
---     if context.cardarea == G.jokers and context.scoring_hand then
---       if context.joker_main and card.ability.extra.mult > 0 then
---         local mult = card.ability.extra.mult
---         return {
---           message = localize{type = 'variable', key = 'a_mult', vars = {mult}}, 
---           colour = G.C.MULT,
---           mult_mod = mult
---         }
---       end
---     end
---     return scaling_evo(self, card, context, "j_sonfive_dusclops", card.ability.extra.mult, self.config.evo_rqmt)
---   end,
---   update = function(self, card, dt)
---     if G.STAGE == G.STAGES.RUN then
---         card.ability.extra.mult = (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.spectral or 0) * card.ability.extra.mult_mod
---     end
---   end,
--- }
+local duskull = {
+  name = "duskull", 
+  poke_custom_prefix = "sonfive",
+  pos = {x = 6, y = 10}, 
+  config = {extra = {mult_mod = 5, mult = 0}, evo_rqmt = 25},
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    info_queue[#info_queue+1] = {set = 'Other', key = 'designed_by', vars = {"vagabond_aeon"}}
+    local mult = card.ability.extra.mult
+    return {vars = {mult, card.ability.extra.mult_mod}}
+  end,
+  rarity = 2, 
+  cost = 5, 
+  stage = "Basic", 
+  ptype = "Psychic",
+  atlas = "pokedex_3",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    if context.cardarea == G.jokers and context.scoring_hand then
+      if context.joker_main and card.ability.extra.mult > 0 then
+        local mult = card.ability.extra.mult
+        return {
+          message = localize{type = 'variable', key = 'a_mult', vars = {mult}}, 
+          colour = G.C.MULT,
+          mult_mod = mult
+        }
+      end
+    end
+    return scaling_evo(self, card, context, "j_sonfive_dusclops", card.ability.extra.mult, self.config.evo_rqmt)
+  end,
+  update = function(self, card, dt)
+    if G.STAGE == G.STAGES.RUN then
+        card.ability.extra.mult = (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.spectral or 0) * card.ability.extra.mult_mod
+    end
+  end,
+      add_to_deck = function(self, card, from_debuff)
+      G.GAME.spectral_rate = G.GAME.spectral_rate + 1
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+      G.GAME.spectral_rate = G.GAME.spectral_rate - 1
+    end,
+}
 
--- local dusclops = {
---   name = "dusclops", 
---   poke_custom_prefix = "sonfive",
---   pos = {x = 7, y = 10}, 
---   config = {extra = {mult_mod = 10, mult = 0}, evo_rqmt = 150},
---   loc_vars = function(self, info_queue, card)
---     type_tooltip(self, info_queue, card)
---     local mult = card.ability.extra.mult
---     return {vars = {mult, card.ability.extra.mult_mod}}
---   end,
---   rarity = 3, 
---   cost = 7, 
---   stage = "One", 
---   ptype = "Psychic",
---   atlas = "pokedex_3",
---   blueprint_compat = true,
---   calculate = function(self, card, context)
---     if context.cardarea == G.jokers and context.scoring_hand then
---       if context.joker_main and card.ability.extra.mult > 0 then
---         local mult = card.ability.extra.mult
---         return {
---           message = localize{type = 'variable', key = 'a_mult', vars = {mult}}, 
---           colour = G.C.MULT,
---           mult_mod = mult
---         }
---       end
---     end
---     return scaling_evo(self, card, context, "j_sonfive_dusknoir", card.ability.extra.mult, self.config.evo_rqmt)
---   end,
---   update = function(self, card, dt)
---     if G.STAGE == G.STAGES.RUN then
---         card.ability.extra.mult = (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.spectral or 0) * card.ability.extra.mult_mod
---     end
---   end,
---     add_to_deck = function(self, card, from_debuff)
---       G.E_MANAGER:add_event(Event({func = function()
---         for k, v in pairs(G.I.CARD) do
---             if v.set_cost then v:set_cost() end
---         end
---         return true end }))
---     end,
---     remove_from_deck = function(self, card, from_debuff)
---         G.E_MANAGER:add_event(Event({func = function()
---         for k, v in pairs(G.I.CARD) do
---             if v.set_cost then v:set_cost() end
---         end
---         return true end }))
---     end,
--- }
+local dusclops = {
+  name = "dusclops", 
+  poke_custom_prefix = "sonfive",
+  pos = {x = 7, y = 10}, 
+  config = {extra = {mult_mod = 10, mult = 0}},
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    info_queue[#info_queue+1] = {set = 'Other', key = 'designed_by', vars = {"vagabond_aeon"}}
+    local mult = card.ability.extra.mult
+    return {vars = {mult, card.ability.extra.mult_mod}}
+  end,
+  rarity = 3, 
+  cost = 7, 
+  item_req = "linkcable",
+  stage = "One", 
+  ptype = "Psychic",
+  atlas = "pokedex_3",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    if context.cardarea == G.jokers and context.scoring_hand then
+      if context.joker_main and card.ability.extra.mult > 0 then
+        local mult = card.ability.extra.mult
+        return {
+          message = localize{type = 'variable', key = 'a_mult', vars = {mult}}, 
+          colour = G.C.MULT,
+          mult_mod = mult
+        }
+      end
+    end
+    return item_evo(self, card, context, "j_sonfive_dusknoir")
+  end,
+  update = function(self, card, dt)
+    if G.STAGE == G.STAGES.RUN then
+        card.ability.extra.mult = (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.spectral or 0) * card.ability.extra.mult_mod
+    end
+  end,
+    add_to_deck = function(self, card, from_debuff)
+      G.GAME.spectral_rate = G.GAME.spectral_rate + 2
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+      G.GAME.spectral_rate = G.GAME.spectral_rate - 2
+    end,
+}
 
--- local dusknoir = {
---   name = "dusknoir", 
---   poke_custom_prefix = "sonfive",
---   pos = {x = 6, y = 6}, 
---   config = {extra = {Xmult_mod = 0.25, Xmult = 1}},
---   loc_vars = function(self, info_queue, card)
---     type_tooltip(self, info_queue, card)
---     local Xmult = 1 + (card.ability.extra.Xmult + card.ability.extra.Xmult_mod)
---     return {vars = {Xmult, card.ability.extra.Xmult_mod}}
---   end,
---   rarity = "poke_safari", 
---   cost = 7, 
---   stage = "Two", 
---   ptype = "Psychic",
---   atlas = "pokedex_4",
---   blueprint_compat = true,
---   calculate = function(self, card, context)
---     if context.cardarea == G.jokers and context.scoring_hand then
---       if context.joker_main then
---         local Xmult = 1 + card.ability.extra.Xmult * card.ability.extra.Xmult_mod
---         return {
---           message = localize{type = 'variable', key = 'a_xmult', vars = {Xmult}}, 
---           colour = G.C.MULT,
---           Xmult_mod = Xmult
---         }
---       end
---     end
---   end,
+local dusknoir = {
+  name = "dusknoir", 
+  poke_custom_prefix = "sonfive",
+  pos = {x = 6, y = 6}, 
+  config = {extra = {Xmult_mod = 0.25, Xmult = 1}},
+  loc_vars = function(self, info_queue, card)
+    type_tooltip(self, info_queue, card)
+    info_queue[#info_queue+1] = {set = 'Other', key = 'designed_by', vars = {"vagabond_aeon"}}
+    local Xmult = card.ability.extra.Xmult
+    return {vars = {Xmult, card.ability.extra.Xmult_mod}}
+  end,
+  rarity = "poke_safari", 
+  cost = 7, 
+  stage = "Two", 
+  ptype = "Psychic",
+  atlas = "pokedex_4",
+  blueprint_compat = true,
+  calculate = function(self, card, context)
+    if context.cardarea == G.jokers and context.scoring_hand then
+      if context.joker_main then
+        local Xmult = 1 + card.ability.extra.Xmult * card.ability.extra.Xmult_mod
+        return {
+          message = localize{type = 'variable', key = 'a_xmult', vars = {Xmult}}, 
+          colour = G.C.MULT,
+          Xmult_mod = Xmult
+        }
+      end
+    end
+  end,
   
---   update = function(self, card, dt)
---     if G.STAGE == G.STAGES.RUN then
---         card.ability.extra.Xmult =  (G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.spectral or 0)
---     end
---   end,
+  update = function(self, card, dt)
+    if G.STAGE == G.STAGES.RUN then
+        card.ability.extra.Xmult =  1 + ((G.GAME.consumeable_usage_total and G.GAME.consumeable_usage_total.spectral or 0) * card.ability.extra.Xmult_mod)
+    end
+  end,
 
---     add_to_deck = function(self, card, from_debuff)
---       G.E_MANAGER:add_event(Event({func = function()
---         for k, v in pairs(G.I.CARD) do
---             if v.set_cost then v:set_cost() end
---         end
---         return true end }))
---     end,
---     remove_from_deck = function(self, card, from_debuff)
---         G.E_MANAGER:add_event(Event({func = function()
---         for k, v in pairs(G.I.CARD) do
---             if v.set_cost then v:set_cost() end
---         end
---         return true end }))
---     end,
--- }
+    add_to_deck = function(self, card, from_debuff)
+      G.GAME.spectral_rate = G.GAME.spectral_rate + 3
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+      G.GAME.spectral_rate = G.GAME.spectral_rate - 3
+    end,
+}
 
 -- local slugma={
 --   name = "slugma",
@@ -237,36 +231,121 @@
 --   end
 -- }
 
-local meltan = {
-  name = "meltan",
+-- Magmar 126
+local vullaby ={
+  name = "vullaby", 
   poke_custom_prefix = "sonfive",
-  pos = {x = 4, y = 9},
-    soul_pos = {x = 5, y = 9},
+  pos = {x = 9, y = 9}, 
+  config = {extra = {mult = 0, mult_mod = 3, flush = 0, evo_rqmt = 1}},
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    return {vars = {center.ability.extra.mult}}
+  end,
+  rarity = 3, 
+  cost = 7, 
+  stage = "Basic", 
+  ptype = "Colorless",
+  atlas = "pokedex_5",
+  blueprint_compat = false,
+  calculate = function(self, card, context)
+    local abbr = card.ability.extra
+    if context.first_hand_drawn then
+      local eval = function() return G.GAME.current_round.hands_played == 0 end
+      juice_card_until(card, eval, true)
+    end
+    if context.individual and context.cardarea == G.play and context.scoring_hand and G.GAME.current_round.hands_played == 0 then
+      for i = 1, #context.scoring_hand do
+        SMODS.debuff_card(context.scoring_hand[i], true, card.config.center.key)
+        abbr.mult = abbr.mult + abbr.mult_mod
+      end
+        elseif context.joker_main then
+          if context.individual and context.cardarea == G.play and #context.full_hand == 5 then
+            local count = 0
+            for i = 1, #context.full_hand do 
+              if context.full_hand[i].debuff then 
+                count = count + 1
+                if count == 5 then 
+                  card.ability.extra.flush = 1
+                end
+              end            
+          end
+        end
+        return {
+          message = localize{type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult}}, 
+          colour = G.C.MULT,
+          mult_mod = card.ability.extra.mult
+        }
+      end
+    return scaling_evo(self, card, context, "j_sonfive_mandibuzz", card.ability.extra.flush, card.ability.extra.evo_rqmt)
+  end
+}
+
+local mandibuzz ={
+  name = "mandibuzz", 
+  poke_custom_prefix = "sonfive",
+  pos = {x = 10, y = 9}, 
+  config = {extra = {mult = 0, mult_mod = 3}},
+  loc_vars = function(self, info_queue, center)
+    type_tooltip(self, info_queue, center)
+    return {vars = {center.ability.extra.mult}}
+  end,
+  rarity = 3, 
+  cost = 7, 
+  stage = "One", 
+  ptype = "Colorless",
+  atlas = "pokedex_5",
+  blueprint_compat = false,
+  calculate = function(self, card, context)
+    local abbr = card.ability.extra
+    if context.first_hand_drawn then
+      local eval = function() return G.GAME.current_round.hands_played == 0 end
+      juice_card_until(card, eval, true)
+    end
+    if context.individual and context.cardarea == G.play and context.scoring_hand and G.GAME.current_round.hands_played == 0 then
+      for i = 1, #context.scoring_hand do
+        SMODS.debuff_card(context.scoring_hand[i], true, card.config.center.key)
+        abbr.mult = abbr.mult + abbr.mult_mod
+
+      end
+      elseif context.joker_main then
+          return {
+            message = localize{type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult}}, 
+            colour = G.C.MULT,
+            mult_mod = card.ability.extra.mult
+          }
+      end
+  end
+}
+
+local meltan = { 
+  name = "meltan", 
+  poke_custom_prefix = "sonfive", -- This is your mod prefix
+  pos = {x = 4, y = 9}, -- This is the sprite position on the atlas, where (0, 0) is the top left sprite 
+  soul_pos = {x = 5, y = 9}, -- This is the sprite position for the floating sprite, for Legendary Jokers
   config = {extra = {chips = 0, count = 0, evo_rqmt = 1}},
   loc_vars = function(self, info_queue, card)
-    type_tooltip(self, info_queue, card)
+    type_tooltip(self, info_queue, card) 
     info_queue[#info_queue+1] = {set = 'Other', key = 'designed_by', vars = {"Sonfive"}}
-		return {vars = {card.ability.extra.count, card.ability.extra.evo_rqmt}}
+		return {vars = {card.ability.extra.count, card.ability.extra.evo_rqmt}} -- Here you can reference values in the Joker description
   end,
-  rarity = 4,
-  cost = 8,
-  stage = "Legendary",
-  ptype = "Metal",
-  atlas = "pokedex_7",
-  blueprint_compat = true,
+  rarity = 4, -- 1 is Common, 2 is Uncommon, 3 is Rare, 4 is Legendary. Use "poke_safari" for Safari 
+  cost = 20, -- This is the purchase cost, half is the sell cost
+  stage = "Legendary", -- "Baby", "Basic", "One", "Two", "Legendary", "Mega"
+  ptype = "Metal", -- Pokemon's type
+  atlas = "pokedex_7", -- This is the spritesheet that the pokemons sprite is located on
+  blueprint_compat = true, -- This decides whether Blueprint shows as compatible or not
   
   calculate = function(self, card, context)
     local abbr = card.ability.extra
     abbr.count = (abbr.energy_count or 0) + (abbr.c_energy_count or 0) + (abbr.negative_energy_count or 0) + (abbr.negative_c_energy_count or 0)
     if context.setting_blind then
-        if not from_debuff and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+        if not from_debuff then
             local _card = create_card('Energy', G.consumeables, nil, nil, nil, nil, 'c_poke_metal_energy')
             local edition = {negative = true}
             _card:set_edition(edition, true)
             _card:add_to_deck()
             G.consumeables:emplace(_card)
             card_eval_status_text(_card, 'extra', nil, nil, nil, {message = localize('poke_plus_pokeitem'), colour = G.C.FILTER})
-            return true
         end
     end
     return scaling_evo(self, card, context, "j_sonfive_melmetal", card.ability.extra.count, card.ability.extra.evo_rqmt)
@@ -285,13 +364,12 @@ local melmetal = {
 		return {vars = {math.max((10*(2^((#find_joker('melmetal') + #find_joker('meltan')) - 1))), 10), card.ability.extra.Xmult_multi, (#find_joker('metal_energy')*card.ability.extra.Xmult_multi), #find_joker('metal_energy')}}
   end,
   rarity = 4,
-  cost = 8,
+  cost = 20,
   stage = "Legendary",
   ptype = "Metal",
   atlas = "pokedex_7",
   blueprint_compat = true,
   custom_pool_func = true,
-  aux_poke = true,
   in_pool = function(self)
     return false
   end,
@@ -470,7 +548,7 @@ local garganacl = {
   end
 }
 
-list = {meltan, melmetal, nacli, naclstack, garganacl}
+list = { meltan, melmetal, nacli, naclstack, garganacl}
 
 return {name = "PokermonPlus1", 
 list = list
