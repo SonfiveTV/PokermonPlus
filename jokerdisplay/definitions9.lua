b7da49279c9e2ae632fe486a7813f2f4ec3d1de4
SonfiveTV <sonfive.jt@gmail.com>
2025-07-03 21:00:03 +0100

    Add files via upload

--- jokerdisplay/definitions9.lua
+++ jokerdisplay/definitions9.lua
@@ -1,5 +1,26 @@
 local jd_def = JokerDisplay.Definitions
 
+jd_def["j_sonfive_shedinja"] = {
+    text = {
+        { text = "+$",                              colour = G.C.GOLD },
+        { ref_table = "card.ability.extra",        ref_value = "money", colour = G.C.GOLD },
+    },
+    reminder_text = {
+            {text = "["},
+            { ref_table = "card.joker_display_values", ref_value = "nature1",},
+            { text = ", " },
+            { ref_table = "card.joker_display_values", ref_value = "nature2",},
+            { text = ", "},
+            { ref_table = "card.joker_display_values", ref_value = "nature3",},
+            {text = "]"},
+    },
+    calc_function = function(card)
+        card.joker_display_values.nature1 = localize(card.ability.extra.targets[1].value, 'ranks')
+        card.joker_display_values.nature2 = localize(card.ability.extra.targets[2].value, 'ranks')
+        card.joker_display_values.nature3 = localize(card.ability.extra.targets[3].value, 'ranks')
+    end
+}
+
 
 jd_def["j_sonfive_nacli"] = {
     text = {
