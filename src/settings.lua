39171b699a6b9ab6bddc5fdcfa31ac0d33ad735f
SonfiveTV <sonfive.jt@gmail.com>
2025-10-24 22:05:26 +0100

    config fix

--- src/settings.lua
+++ src/settings.lua
@@ -1,11 +1,11 @@
 SMODS.current_mod.config_tab = function() 
     local nodes = {}
 
-    for i = 1, #evo_lines do
+    for i = 1, #sonfive_evo_lines do
         table.insert(nodes, create_toggle({
             label = localize(sonfive_evo_lines[i].."_line"),
             ref_table = sonfive_config,
-            ref_value = evo_lines[i]
+            ref_value = sonfive_evo_lines[i]
         }))
     end
 
