46225ef9f97b88fb86f8a3a4fd3d14dd6071e5f0
SonfiveTV <sonfive.jt@gmail.com>
2025-10-25 18:40:44 +0100

    croagunk fix

--- src/pokemon/453_croagunk_line.lua
+++ src/pokemon/453_croagunk_line.lua
@@ -11,7 +11,7 @@ local croagunk = {
   loc_vars = function(self, info_queue, card)
     type_tooltip(self, info_queue, card)
     local a = card.ability.extra
-    local retriggers, evo_rqmt, last_tarot = a.retriggers, a.evo_rqmt, context.consumeable.config.center_key
+    local retriggers, evo_rqmt, last_tarot = a.retriggers, a.evo_rqmt, a.previous_tarot
     return {vars = {retriggers, last_tarot, evo_rqmt}}
   end,
 
