efde43b079031000e25d4d36e8ce1f529bc67686
SonfiveTV <sonfive.jt@gmail.com>
2025-10-25 17:49:44 +0100

    croagunk tweak

--- src/pokemon/453_croagunk_line.lua
+++ src/pokemon/453_croagunk_line.lua
@@ -3,8 +3,8 @@ local croagunk = {
   config = {
     extra = {
       reset = true,
-      retriggers = 0, 
-      previous_tarot = "Not set!", 
+      retriggers = 0,
+      previous_tarot = "Not set!",
       evo_rqmt = 5
     }
   },
@@ -12,18 +12,18 @@ local croagunk = {
     type_tooltip(self, info_queue, card)
     local a = card.ability.extra
     local retriggers, evo_rqmt, last_tarot = a.retriggers, a.evo_rqmt
-    if a.previous_tarot and G.P_CARDS[a.previous_tarot] then
+
+    -- Display last used Tarot name if available
+    if type(a.previous_tarot) == "string" and G.P_CARDS[a.previous_tarot] then
       local prev = G.P_CARDS[a.previous_tarot]
       last_tarot = localize { type = 'name_text', key = prev.key, set = prev.set }
     else
       last_tarot = localize('poke_none')
     end
-    return {vars = {
-      retriggers, 
-      last_tarot,
-      evo_rqmt
-    }}
+
+    return {vars = {retriggers, last_tarot, evo_rqmt}}
   end,
+
   designer = "Sonfive",
   rarity = 2,
   cost = 5,
@@ -31,9 +31,12 @@ local croagunk = {
   ptype = "Dark",
   blueprint_compat = true,
   poke_custom_values_to_keep = {"retriggers", "previous_tarot"},
+
   calculate = function(self, card, context)
     local a = card.ability.extra
-        if context.using_consumeable and not context.blueprint and context.consumeable and context.consumeable.ability then
+
+    -- When a Tarot consumable is used
+    if context.using_consumeable and not context.blueprint and context.consumeable and context.consumeable.ability then
       local ability = context.consumeable.ability
       local key = ability.key or "unknown"
 
@@ -47,21 +50,32 @@ local croagunk = {
       a.reset = false
     end
 
+    -- When a Purple-sealed card is played, retrigger it
     if context.repetition and context.cardarea == G.play and not context.other_card.debuff then
       if context.other_card.seal == "Purple" then
-          return {
-            message = localize('k_again_ex'),
-            repetitions = a.retriggers,
-            card = card
-          }
+        return {
+          message = localize('k_again_ex'),
+          repetitions = a.retriggers,
+          card = card
+        }
       end
     end
-    if (context.end_of_round) and not context.blueprint then
-      a.retriggers = a.reset and 0 or a.retriggers
+
+    -- Reset retrigger counter at end of round
+    if context.end_of_round and not context.blueprint then
+      if a.reset then
+        a.retriggers = 0
+        a.previous_tarot = "Not set!"
+      else
+        a.reset = true
+      end
     end
+
+    -- Handle evolution scaling logic
     return scaling_evo(self, card, context, "j_sonfive_toxicroak", a.retriggers, a.evo_rqmt)
   end
 }
+
 
 local toxicroak = {
   name = "toxicroak",
