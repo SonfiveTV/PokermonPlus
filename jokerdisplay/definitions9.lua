4b569030ea71dcb5040f65e06ba3c63c261915e8
SonfiveTV <sonfive.jt@gmail.com>
2025-11-07 17:14:22 +0000

    JokerDisplay support

--- jokerdisplay/definitions9.lua
+++ jokerdisplay/definitions9.lua
@@ -1,5 +1,23 @@
 local jd_def = JokerDisplay.Definitions
 
+jd_def["j_sonfive_nincada"] = {
+    text = {
+        { text = "+" },
+        { ref_table = "card.ability.extra", ref_value = "mult", retrigger_type = "mult", colour = G.C.MULT },
+        { text = " or " },
+        { ref_table = "card.ability.extra", ref_value = "chips", retrigger_type = "mult", colour = G.C.CHIPS },
+    },
+}
+
+jd_def["j_sonfive_ninjask"] = {
+    text = {
+        { text = "+" },
+        { ref_table = "card.ability.extra", ref_value = "mult", retrigger_type = "mult", colour = G.C.MULT },
+        { text = " & " },
+        { ref_table = "card.ability.extra", ref_value = "chips", retrigger_type = "mult", colour = G.C.CHIPS },
+    },
+}
+
 jd_def["j_sonfive_shedinja"] = {
     reminder_text = {
             {text = "["},
@@ -18,6 +36,92 @@ jd_def["j_sonfive_shedinja"] = {
     end
 }
 
+jd_def["j_sonfive_helioptile"] = {
+  text = {
+    { text = "Min: ", colour = G.C.GREY },
+    { text = "+$", colour = G.C.GOLD },
+    { ref_table = "card.joker_display_values", ref_value = "money", colour = G.C.GOLD },
+    { text = " Max: ", colour = G.C.GREY },
+    { text = "+$", colour = G.C.GOLD },
+    { ref_table = "card.joker_display_values", ref_value = "money2", colour = G.C.GOLD }
+  },
+  reminder_text = {
+    { text = "(" },
+    { ref_table = "card.joker_display_values", ref_value = "localized_text", colour = lighten(G.C.SUITS["Hearts"], 0.35) },
+    { text = ")" }
+  },
+  extra = {
+    {
+      { text = "(", colour = G.C.GREEN, scale = 0.3 },
+      { ref_table = "card.joker_display_values", ref_value = "odds", colour = G.C.GREEN, scale = 0.3 },
+      { text = ")", colour = G.C.GREEN, scale = 0.3 },
+    },
+  },
+  calc_function = function(card)
+    local count = 0
+    if G.play then
+      local text, _, scoring_hand = JokerDisplay.evaluate_hand()
+      if text ~= 'Unknown' then
+        for _, scoring_card in pairs(scoring_hand) do
+          if scoring_card:is_suit("Hearts") then
+            count = count + JokerDisplay.calculate_card_triggers(scoring_card, scoring_hand)
+          end
+        end
+      end
+    end
+    card.joker_display_values.money = count * card.ability.extra.money_mod
+    card.joker_display_values.money2 = count * card.ability.extra.money_mod * 2
+    card.joker_display_values.localized_text = localize("Hearts", 'suits_plural')
+    local num, dem = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator, 'helioptile')
+    card.joker_display_values.odds = localize { type = 'variable', key = "jdis_odds", vars = { num, dem } }
+    end
+}
+
+jd_def["j_sonfive_heliolisk"] = {
+  text = {
+    { text = "Min: ", colour = G.C.GREY },
+    { text = "+$", colour = G.C.GOLD },
+    { ref_table = "card.joker_display_values", ref_value = "money", colour = G.C.GOLD },
+    { text = " Max: ", colour = G.C.GREY },
+    { text = "+$", colour = G.C.GOLD },
+    { ref_table = "card.joker_display_values", ref_value = "money2", colour = G.C.GOLD }
+  },
+  reminder_text = {
+    { text = "(" },
+    { ref_table = "card.joker_display_values", ref_value = "localized_text", colour = lighten(G.C.SUITS["Hearts"], 0.35) },
+    { text = ")" }
+  },
+  extra = {
+    {
+      { text = "(", colour = G.C.GREEN, scale = 0.3 },
+      { ref_table = "card.joker_display_values", ref_value = "odds", colour = G.C.GREEN, scale = 0.3 },
+      { text = ")", colour = G.C.GREEN, scale = 0.3 },
+    },
+  },
+  calc_function = function(card)
+    local count = 0
+    if G.play then
+      local text, _, scoring_hand = JokerDisplay.evaluate_hand()
+      if text ~= 'Unknown' then
+        for _, scoring_card in pairs(scoring_hand) do
+          if scoring_card:is_suit("Hearts") then
+            count = count + JokerDisplay.calculate_card_triggers(scoring_card, scoring_hand)
+          end
+        end
+      end
+    end
+    card.joker_display_values.money = 0
+    if count == 0 then
+        card.joker_display_values.money2 = 0
+    else
+        card.joker_display_values.money2 = card.ability.extra.money_mod * 2^(count - 1)
+    end
+    card.joker_display_values.localized_text = localize("Hearts", 'suits_plural')
+    local num, dem = SMODS.get_probability_vars(card, card.ability.extra.numerator, card.ability.extra.denominator, 'heliolisk')
+    card.joker_display_values.odds = localize { type = 'variable', key = "jdis_odds", vars = { num, dem } }
+    end
+}
+
 jd_def["j_sonfive_meltan"] = {
     retrigger_function = function(playing_card, scoring_hand, held_in_hand, joker_card)
         return held_in_hand and playing_card.ability.effect == "Steel Card" and (joker_card.ability.extra.retriggers * JokerDisplay.calculate_joker_triggers(joker_card)) or 0
@@ -31,6 +135,35 @@ jd_def["j_sonfive_melmetal"] = {
     end
 }
 
+jd_def["j_sonfive_duraludon"] = {
+    text = {
+        {
+            border_nodes = {
+                { text = "X" },
+                { ref_table = "card.joker_display_values", ref_value = "x_mult", retrigger_type = "exp" },
+            },
+        },
+    },
+    calc_function = function(card)
+        card.joker_display_values.x_mult = ((card.ability.extra.current_hand == "odd") and card.ability.extra.Xmult) or 1
+    end
+    
+}
+
+jd_def["j_sonfive_archaludon"] = {
+    text = {
+        {
+            border_nodes = {
+                { text = "X" },
+                { ref_table = "card.joker_display_values", ref_value = "x_mult", retrigger_type = "exp" },
+            },
+        },
+    },
+    calc_function = function(card)
+        card.joker_display_values.x_mult = ((card.ability.extra.current_hand == "even") and card.ability.extra.Xmult) or 1
+    end
+    
+}
 
 jd_def["j_sonfive_nacli"] = {
     text = {
@@ -64,4 +197,48 @@ jd_def["j_sonfive_garganacl"] = {
         },
     },
 
+}
+
+jd_def["j_sonfive_cetoddle"] = {
+    text = {
+        {
+            border_nodes = {
+                { text = "X" },
+                { ref_table = "card.ability.extra", ref_value = "Xmult", retrigger_type = "exp" },
+            },
+        },
+    },
+}
+
+jd_def["j_sonfive_cetitan"] = {
+    text = {
+        {
+            border_nodes = {
+                { text = "X" },
+                { ref_table = "card.joker_display_values", ref_value = "x_mult", retrigger_type = "exp" },
+            },
+        },
+    },
+  calc_function = function(card)
+        local playing_hand = next(G.play.cards)
+        local count = 0
+        local glass_count = 0
+        local double = false
+        for _, playing_card in ipairs(G.hand.cards) do
+            if playing_hand or not playing_card.highlighted then
+                if playing_card then
+                    count = count + 1
+                    if playing_card and playing_card.ability and playing_card.ability.effect and playing_card.ability.effect == "Glass Card" then
+                        glass_count = glass_count + 1
+                    end
+                end
+            end
+        end
+        if glass_count == count then 
+            double = true
+        else
+            double = false
+        end
+        card.joker_display_values.x_mult = (double and (card.ability.extra.Xmult * 2)) or card.ability.extra.Xmult
+    end
 }
--- main.lua
+++ main.lua
@@ -4,6 +4,20 @@ if (SMODS.Mods["Pokermon"] or {}).can_load then
     pokermon_config = SMODS.Mods["Pokermon"].config
 end
 
+if (SMODS.Mods["JokerDisplay"] or {}).can_load then
+  local jokerdisplays = NFS.getDirectoryItems(mod_dir.."jokerdisplay")
+
+  for _, file in ipairs(jokerdisplays) do
+    sendDebugMessage ("The file is: "..file)
+    local helper, load_error = SMODS.load_file("jokerdisplay/"..file)
+    if load_error then
+      sendDebugMessage ("The error is: "..load_error)
+    else
+      helper()
+    end
+  end
+end
+
 --Load files
 local function load_files()
   local files = {
