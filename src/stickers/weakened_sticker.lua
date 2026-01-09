b44ed7d87ad1df5f89751612d71c702ec5c8bdd3
SonfiveTV <sonfive.jt@gmail.com>
2026-01-09 07:24:01 +0000

    removed weakened message

--- src/stickers/weakened_sticker.lua
+++ src/stickers/weakened_sticker.lua
@@ -11,7 +11,7 @@ local weakened = {
     card.ability[self.key] = val
     if card.ability.extra then
       if type(card.ability.extra) == "table" then
-        increment_energy(card, get_type(card), -1)
+        increment_energy(card, get_type(card), -1, true)
         card.ability.extra.e_limit_up = card.ability.extra.e_limit_up and (card.ability.extra.e_limit_up - 1) or -1
       end
     end
