33c596cf8ed44a121a0f8f909b4dee27c0352690
SonfiveTV <sonfive.jt@gmail.com>
2026-05-13 17:39:01 +0100

    retired Prophetic and Rogue Deck

--- src/backs/backs.lua
+++ src/backs/backs.lua
@@ -158,7 +158,7 @@ local pyritedeck = {
   end,
 }
 
-local dList = {virtuousdeck, propheticdeck, shinydeck, roguedeck, voiddeck, pearlescentdeck}
+local dList = {virtuousdeck, shinydeck, voiddeck, pearlescentdeck}
 return {name = "Back",
         list = dList}
 
